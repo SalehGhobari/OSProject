783ce50792cf533a67089313769b257d1ebd368e
SalehGhobari <SalehGhobari@outlook.com>
2025-05-29 22:22:04 +0000

    Hello World

---
+++ scheduler_node/build/scheduler_node/CMakeFiles/scheduler_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scheduler_node.dir/src/scheduler_node.cpp.o"
+  "CMakeFiles/scheduler_node.dir/src/scheduler_node.cpp.o.d"
+  "scheduler_node"
+  "scheduler_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scheduler_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ scheduler_node/build/scheduler_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/saleh/Desktop/OSProject/scheduler_node
+# Build directory: /home/saleh/Desktop/OSProject/scheduler_node/build/scheduler_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
